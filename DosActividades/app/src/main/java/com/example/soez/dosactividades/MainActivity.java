1172d28a72659ba14796abc16efd1db1b86237a3
Vicente Sáez Rufete <SOEZ@veraw78-227.wi-fi.upv.es>
2017-02-21 12:13:58 +0100

    Boton Principal pulsas y sale logcat

--- DosActividades/app/src/main/java/com/example/soez/dosactividades/MainActivity.java
+++ DosActividades/app/src/main/java/com/example/soez/dosactividades/MainActivity.java
@@ -2,14 +2,35 @@ package com.example.soez.dosactividades;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button botonPrincipal;
+    private TextView textoResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d("Vicente.DosActividades","Inicio onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        botonPrincipal = (Button) findViewById(R.id.botonPrincipal);
+        textoResultado = (TextView) findViewById(R.id.MuestraResultado);
+
+        botonPrincipal.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Log.d("Vicente.DosActividades","Boton principal pulsado");
+                
+            }
+        });
     }
+
+
 }
