b29bd1c55b0108dce6edd12e81bd6802665e07bd
Vicente Sáez Rufete <SOEZ@veraw78-227.wi-fi.upv.es>
2017-02-28 10:13:51 +0100

    Menú creado que lleva a cada una de las 3 actividades

---
+++ TresActividades/app/src/main/java/com/example/soez/tresactividades/Main2Activity.java
@@ -0,0 +1,73 @@
+package com.example.soez.tresactividades;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+    private int Actividad1,Actividad2,Actividad3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Actividad1=1;
+        Actividad2=2;
+        Actividad3=3;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // para crear los menus (mejor definir constantes en vez 1234, 1235)
+
+        menu.add(0, Actividad1, 0, "Actividad1").setIcon(android.R.drawable.ic_popup_sync);
+        menu.add(0, Actividad2, 0, "Actividad2").setIcon(android.R.drawable.ic_popup_sync);
+        menu.add(0, Actividad3, 0, "Actividad3").setIcon(android.R.drawable.ic_popup_sync);
+
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // callback de una opcion del boton de menu
+        if (item.getItemId() == Actividad1) {
+
+            Log.d("Vicente.TresActividades","Actividad1 comenzada");
+            Toast.makeText(this, "Actividad1", Toast.LENGTH_LONG).show();
+
+            Intent intencion1 = new Intent(Main2Activity.this, MainActivity.class);
+            intencion1.putExtra("VengoDe","MainActivity");
+            Main2Activity.super.startActivity(intencion1);
+
+        } else if (item.getItemId() == Actividad2) {
+
+            Log.d("Vicente.TresActividades","Me quedo en esta actividad");
+            Toast.makeText(this, "Actividad2", Toast.LENGTH_LONG).show();
+
+
+
+        }
+        else if(item.getItemId() == Actividad3){
+
+            Log.d("Vicente.TresActividades","Actividad3 comenzada");
+            Toast.makeText(this, "Actividad3", Toast.LENGTH_LONG).show();
+
+            Intent intencion3 = new Intent(Main2Activity.this, Main3Activity.class);
+            intencion3.putExtra("VengoDe","MainActivity");
+            Main2Activity.super.startActivity(intencion3);
+
+        }
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        // deja sin efecto la tecla física BACK (volver)
+
+        return;
+    }
+}
---
+++ TresActividades/app/src/main/java/com/example/soez/tresactividades/Main3Activity.java
@@ -0,0 +1,71 @@
+package com.example.soez.tresactividades;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class Main3Activity extends AppCompatActivity {
+    private int Actividad1,Actividad2,Actividad3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        Actividad1=1;
+        Actividad2=2;
+        Actividad3=3;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // para crear los menus (mejor definir constantes en vez 1234, 1235)
+
+        menu.add(0, Actividad1, 0, "Actividad1").setIcon(android.R.drawable.ic_popup_sync);
+        menu.add(0, Actividad2, 0, "Actividad2").setIcon(android.R.drawable.ic_popup_sync);
+        menu.add(0, Actividad3, 0, "Actividad3").setIcon(android.R.drawable.ic_popup_sync);
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // callback de una opcion del boton de menu
+        if (item.getItemId() == Actividad1) {
+
+            Log.d("Vicente.TresActividades","Actividad1 comenzada");
+            Toast.makeText(this, "Actividad1", Toast.LENGTH_LONG).show();
+
+            Intent intencion1 = new Intent(Main3Activity.this, MainActivity.class);
+            intencion1.putExtra("VengoDe","MainActivity");
+            Main3Activity.super.startActivity(intencion1);
+
+        } else if (item.getItemId() == Actividad2) {
+
+            Log.d("Vicente.TresActividades","Actividad2 comenzada");
+            Toast.makeText(this, "Actividad2", Toast.LENGTH_LONG).show();
+
+            Intent intencion2 = new Intent(Main3Activity.this, Main2Activity.class);
+            intencion2.putExtra("VengoDe","MainActivity");
+            Main3Activity.super.startActivity(intencion2);
+
+        }
+        else if(item.getItemId() == Actividad3){
+
+            Log.d("Vicente.TresActividades","Me quedo en esta actividad");
+            Toast.makeText(this, "Actividad3", Toast.LENGTH_LONG).show();
+
+
+        }
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        // deja sin efecto la tecla física BACK (volver)
+
+        return;
+    }
+}
